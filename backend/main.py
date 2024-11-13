621d0ddbfb9b2236dd019d8c8b7e2e2e8b18a0aa
Aashish Gupta <ashuguptahere@gmail.com>
2024-11-13 18:12:31 +0530

    fix: formatted code with ruff

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import numpy as np
-from PIL import Image
+import os
 import io
 import requests
+import numpy as np
+from PIL import Image
+from flask_cors import CORS
+from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
-import os
 
-MODEL_URL = 'https://qleapmodel.s3.amazonaws.com/model_100.h5'
-MODEL_DIR = os.path.join(os.getcwd(), 'model')
-MODEL_PATH = os.path.join(MODEL_DIR, 'model_100.h5')
+MODEL_URL = "https://qleapmodel.s3.amazonaws.com/model_100.h5"
+MODEL_DIR = os.path.join(os.getcwd(), "model")
+MODEL_PATH = os.path.join(MODEL_DIR, "model_100.keras")
 
 os.makedirs(MODEL_DIR, exist_ok=True)
 
@@ -17,22 +17,23 @@ os.makedirs(MODEL_DIR, exist_ok=True)
 if not os.path.exists(MODEL_PATH):
     response = requests.get(MODEL_URL)
     if response.status_code == 200:
-        print('Downloading the model from S3')
-        with open(MODEL_PATH, 'wb') as f:    
+        print("Downloading the model from S3")
+        with open(MODEL_PATH, "wb") as f:
             f.write(response.content)
     else:
-        raise ValueError('Failed to download the model from S3')
+        raise ValueError("Failed to download the model from S3")
 else:
-    print('Model already exists. Skipping download.')
+    print("Model already exists. Skipping download.")
 
 # Load the model
 model = load_model(MODEL_PATH)
 if model is None:
-    raise ValueError('Model not found')
+    raise ValueError("Model not found")
 
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
+
 
 def load_and_preprocess_image(img):
     img = img.convert("RGB")
@@ -41,6 +42,7 @@ def load_and_preprocess_image(img):
     img_array = img_array / 255.0
     return img_array
 
+
 def predict_image_class(img_array):
     img_array = np.expand_dims(img_array, axis=0)
     predictions = model.predict(img_array)
@@ -48,17 +50,19 @@ def predict_image_class(img_array):
     class_probabilities = predictions[0]
     return predicted_class, class_probabilities
 
-@app.route('/')
+
+@app.route("/")
 def main():
     return "Flask app is running"
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
     if file:
         img_bytes = file.read()
         img = Image.open(io.BytesIO(img_bytes))
@@ -66,17 +70,20 @@ def predict():
 
         predicted_class, class_probabilities = predict_image_class(img_array)
         if int(predicted_class) == 0:
-            class_ = 'Bordered'
+            class_ = "Bordered"
         elif int(predicted_class) == 1:
-            class_ = 'Borderless'
+            class_ = "Borderless"
         elif int(predicted_class) == 2:
-            class_ = 'Row Bordered'
+            class_ = "Row Bordered"
         else:
-            class_ = 'Unknown'
-        return jsonify({
-            'predicted_class': class_,
-            'class_probabilities': class_probabilities.tolist()
-        })
+            class_ = "Unknown"
+        return jsonify(
+            {
+                "predicted_class": class_,
+                "class_probabilities": class_probabilities.tolist(),
+            }
+        )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- main.py
+++ main.py
@@ -8,36 +8,31 @@ from urllib.parse import urlparse
 
 curr = os.getcwd()
 
-frontend_dir = os.path.join(curr, 'frontend')
-backend_dir = os.path.join(curr, 'backend')
-backend_script = 'main.py'
+backend_dir = os.path.join(curr, "backend")
+frontend_dir = os.path.join(curr, "frontend")
 
-npm_path = '/opt/homebrew/lib/node_modules/npm/bin/npm'
+npm_path = "/usr/bin/npm"
 python_path = sys.executable
 
-def create_backend_virtualenv(venv_path):
-    print("Creating backend virtual environment...")
-    subprocess.run([python_path, '-m', 'venv', venv_path], cwd=backend_dir)
-
-def install_backend_requirements(venv_path):
-    print("Installing backend requirements...")
-    pip_executable = os.path.join(venv_path, 'bin', 'pip')
-    subprocess.run([pip_executable, 'install', '-r', os.path.join(backend_dir, 'requirements.txt')], cwd=backend_dir)
 
 def install_frontend_dependencies():
     print("Installing frontend dependencies...")
-    subprocess.run([npm_path, 'install'], cwd=frontend_dir)
+    subprocess.run([npm_path, "install"], cwd=frontend_dir)
+
 
 def start_frontend():
     print("Starting frontend...")
-    frontend_process = subprocess.Popen([npm_path, 'run', 'dev'], cwd=frontend_dir)
+    frontend_process = subprocess.Popen([npm_path, "run", "dev"], cwd=frontend_dir)
     return frontend_process
 
-def start_backend(venv_path):
+
+def start_backend():
     print("Starting backend...")
-    python_executable = os.path.join(venv_path, 'bin', 'python')
-    backend_process = subprocess.Popen([python_executable, backend_script], cwd=backend_dir)
+    backend_process = subprocess.Popen(
+        [python_path, "main.py"], cwd=backend_dir
+    )
     return backend_process
+
 
 def wait_for_process(proc, name):
     try:
@@ -47,12 +42,13 @@ def wait_for_process(proc, name):
         proc.terminate()
         proc.wait()
 
-def is_backend_ready(url='http://localhost:5000'):
+
+def is_backend_ready(url="http://localhost:5000"):
     parsed_url = urlparse(url)
     connection = http.client.HTTPConnection(parsed_url.hostname, parsed_url.port)
-    
+
     try:
-        connection.request('GET', parsed_url.path)
+        connection.request("GET", parsed_url.path)
         response = connection.getresponse()
         return response.status == 200
     except (http.client.HTTPException, ConnectionRefusedError):
@@ -60,30 +56,30 @@ def is_backend_ready(url='http://localhost:5000'):
     finally:
         connection.close()
 
+
 if __name__ == "__main__":
-    
-    backend_venv_path = os.path.join(backend_dir, '.venv')
-    create_backend_virtualenv(backend_venv_path)
-    install_backend_requirements(backend_venv_path)
     install_frontend_dependencies()
-    
-    backend_process = start_backend(backend_venv_path)
-    
+    backend_process = start_backend()
+
     backend_ready = False
     while not backend_ready:
         print("Model is too big to load, please wait for a while...")
         time.sleep(30)
         backend_ready = is_backend_ready()
-    
+
     frontend_process = start_frontend()
 
-    backend_thread = threading.Thread(target=wait_for_process, args=(backend_process, "backend"))
-    frontend_thread = threading.Thread(target=wait_for_process, args=(frontend_process, "frontend"))
-    
+    backend_thread = threading.Thread(
+        target=wait_for_process, args=(backend_process, "backend")
+    )
+    frontend_thread = threading.Thread(
+        target=wait_for_process, args=(frontend_process, "frontend")
+    )
+
     backend_thread.start()
     frontend_thread.start()
-    
+
     backend_thread.join()
     frontend_thread.join()
-    
+
     print("Both processes terminated.")
